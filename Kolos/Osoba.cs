71023fa04253150bcedf56aea1ed6e8d416b9bd4
Sugar <sugarypolecat@gmail.com>
2022-01-18 23:04:04 +0100

    vdf

--- Kolos/Osoba.cs
+++ Kolos/Osoba.cs
@@ -17,7 +17,15 @@ namespace Kolos
         /// <exception cref="FormatException">To sie dzieje jak pesel jest pusty albo nie ma 11 znakow</exception>
         public Osoba(string _imie, string _nazwisko, string _pesel)
         {
+            if (string.IsNullOrEmpty(_imie))
+            {
+                throw new ArgumentNullException();
+            }
             this.imie = _imie;
+            if (string.IsNullOrEmpty(_nazwisko))
+            {
+                throw new ArgumentNullException();
+            }
             this.nazwisko = _nazwisko;
             // jezeli nasz pesel jest pusty lub NULL, LUB nie ma 11 znakow
             if (string.IsNullOrEmpty(_pesel) || _pesel.Length != 11)
